813de7d4c73e05c425d1acef1b169d6f62039185
ddkangfu <ddkangfu@gmail.com>
2017-06-23 10:35:17 +0800

    added: 增加基本设置，创建基础模板

---
+++ chapter03/prototypes.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from django.conf import settings
+
+BASE_DIR = os.path.dirname(__file__)
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='cumx*yy45q&%w@4sg%afb34)^^*z@6w97lj&+pn(fn@4=kskat',
+    ROOT_URLCONF='sitebuilder.urls',
+    MIDDLEWARE_CLASSES=(),
+    INSTALLED_APPS=(
+        'django.contrib.staticfiles',
+        'sitebuilder',
+    ),
+    TEMPLATES=(
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+        },
+    ),
+    STATIC_URL='/static/',
+    SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
+)
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
